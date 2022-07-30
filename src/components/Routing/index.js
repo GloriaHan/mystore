10c4caaecf2c86c61d7b8fec030c219624fb2597
Gloria <hanning_hit@hotmail.com>
2022-07-30 15:18:09 +1000

    fix

--- src/components/Routing/index.js
+++ src/components/Routing/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import Content from "../Content";
 import Homepage from "../Homepage";
+import Content from "../Content";
 
 export default function Routing() {
   return (
