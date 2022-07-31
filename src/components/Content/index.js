c07532ecfa296921bb4263331f74da38fd70682c
Gloria <hanning_hit@hotmail.com>
2022-07-31 14:12:45 +1000

    add env

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,28 +4,19 @@ export default function Content() {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState("");
 
-  const fetchInfo = async () => {
-    const url = `https://fakestoreapi.com/products`;
-    setLoading(true);
-    const res = await fetch(url);
-    const data = await res.json();
-    setLoading(false);
-    setProducts(data);
-    console.log(products);
-
-    return data;
-  };
-  //   useEffect(() => {
-  //     fetchInfo().then((response) => {
-  //       setProducts(response);
-  //     console.log(response);
-
-  //     });
-  //   }, []);
+  useEffect(() => {
+    (async () => {
+      const url = `${process.env.REACT_APP_BASE_URL}/products`;
+      setLoading(true);
+      const res = await fetch(url);
+      const data = await res.json();
+      setLoading(false);
+      setProducts(data);
+    })();
+  }, []);
 
   return (
     <div>
-      <button onClick={() => fetchInfo()}>get info</button>
       {products &&
         products.map((item) => (
           <span key={item.id}>
