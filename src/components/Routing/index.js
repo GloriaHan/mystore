1851500c1f0739c28f6c5ac4b98a93c0ac8cc06c
Gloria <hanning_hit@hotmail.com>
2022-07-31 13:47:35 +1000

    add cart component

--- src/components/Routing/index.js
+++ src/components/Routing/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import Homepage from "../Homepage";
+import Cart from "../Cart";
 import Content from "../Content";
 import Contents from "../Contents";
-import Welcome from "../Welcome";
+import Homepage from "../Homepage";
 import ProdectDetail from "../ProductDetail";
+import Welcome from "../Welcome";
 
 export default function Routing() {
   return (
@@ -19,6 +20,7 @@ export default function Routing() {
           </Route>
 
           <Route path="prodectdetail" element={<ProdectDetail />} />
+          <Route path="cart" element={<Cart />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/" replace />} />
