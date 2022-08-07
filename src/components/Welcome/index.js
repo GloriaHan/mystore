884f418f8f04868c82c5068f24734a9d01d94ae8
Gloria <hanning_hit@hotmail.com>
2022-08-07 17:10:24 +1000

    1

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -9,7 +9,7 @@ export default function Welcome() {
   return (
     <Root>
       <h1> Homepage</h1>
-      <h2>WELCOME TO THE OUR STORE !</h2>
+      <h2>WELCOME TO MYSTORE !</h2>
       <h3>It's time to treat yourself.</h3>
       <div>
         <Button
